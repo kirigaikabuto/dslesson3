9c89a9401266e3be7f6644eb52df3b2fe1a69a55
Your Name <tleugazy98.com>
2020-04-17 17:53:07 +0600

    adsdas

---
+++ lesson1/10.py
@@ -0,0 +1,7 @@
+#есть 5 яблок 
+# груш в 2 раза больше чем яблок
+# бананов на 3 больше чем груш
+apples = 5
+grush = apples*2
+bananas = grush + 3
+print(apples,grush,bananas)
--- lesson1/9.py
+++ lesson1/9.py
@@ -1,4 +1,8 @@
 a = input("Введите первое число:")
 b = input("Введите второе число:")
+a = int(a)
+b = int(b)
 c=a+b
-print(c)
+print(f"Sum of two number a={a} and b ={b} is equal={c}")
+s= "Sum of two number a={} and b ={} is equal={}".format(a,b,c)
+print(s)
