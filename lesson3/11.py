83f8b93183e16af7858cc537c63ca4307f60b277
Your Name <tleugazy98.com>
2020-04-21 17:32:39 +0600

    asdsad

---
+++ lesson3/11.py
@@ -0,0 +1,13 @@
+#ввод данных в массив
+n = int(input("Колво элементов:"))
+arr=[]
+for i in range(n):
+    num = int(input())
+    arr.append(num)
+print(arr)
+# 5
+# 100
+# -2
+# 30
+# 41
+# -5
---
+++ lesson3/8.py
@@ -0,0 +1,12 @@
+arr=[100,-2,123,344,55,600]
+n = len(arr)
+# print(arr)
+# arr[2] = 20
+# print(arr)
+#range(start,end,step)
+#i its value
+for i in arr:
+    print(i)
+#i its index,arr[i] value
+for i in range(n):
+    print(i,arr[i])
