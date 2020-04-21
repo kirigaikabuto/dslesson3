6a9d361e6a3c923a43be576c10b0c53c5318f8e4
Your Name <tleugazy98.com>
2020-04-21 16:30:37 +0600

    adad

---
+++ lesson3/1.py
@@ -0,0 +1,25 @@
+#array or list
+#создание листа
+arr = [900,32,300,41,50,9]
+#вывод элементов листа
+# print(arr[0])
+# print(arr[4])
+#ариф операции
+sumi = arr[0]+arr[1]+arr[2]+arr[3]+arr[4]+arr[5]
+print(f"Sum of array is {sumi}")
+#добавление элементов в массив
+print(arr)
+arr.append(100)
+print(arr)
+#колво элементов в массиве
+n = len(arr)
+print(f"The length of array is {n}")
+# arr 
+# value  index(address)
+# 900    0
+# 32     1
+# 300    2 
+# 41     3 
+# 50     4
+
+
---
+++ lesson3/2.py
@@ -0,0 +1,7 @@
+arr=[1,2,0,-1]
+for i in arr:
+    #step
+    print(i)
+    print(i+1)
+    print(i+2)
+print("endfor")
