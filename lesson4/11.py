c9b21938220d805511386a5f1f81cd4bad35e0ef
Your Name <tleugazy98.com>
2020-04-23 17:52:05 +0600

    asdsd

---
+++ lesson4/11.py
@@ -0,0 +1,15 @@
+arr=[1,1,2,3,4,1,2,3,4,5]
+#1
+# unique = set(arr)
+# unique = list(unique)
+# print(unique)
+#2
+# if 0 not in arr:
+#     print("ok")
+# else:
+#     print("no")
+# unique=[]
+# for i in arr:
+#     if i not in unique:
+#         unique.append(i)
+# print(unique)
