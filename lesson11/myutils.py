ea475c5eca50a6959a3593af61cf5a1baaf88052
Your Name <tleugazy98.com>
2020-05-11 17:52:11 +0600

    dsds

--- lesson11/myutils.py
+++ lesson11/myutils.py
@@ -1,3 +1,19 @@
 def printarr(a):
     for i in a:
         print(i)
+
+def find_max(a):
+    maxi = a[0]
+    for i in a:
+        if i>maxi:
+            maxi = i
+    return maxi
+def find_real_age(d):
+    return 2020-d['year']
+
+def find_real_age1(year):
+    return 2020-year
+
+
+def some_find(d,key):
+    return d[key]
