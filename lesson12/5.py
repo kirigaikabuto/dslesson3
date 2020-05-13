6511cd1767dfb43304174482b7057a0270d8b54c
Yerassyl Tleugazy <ytleugazy@dar.kz>
2020-05-13 17:37:07 +0600

    sdsds

--- lesson12/5.py
+++ lesson12/5.py
@@ -16,10 +16,10 @@ arr=[
         "marks":[1,2,3,4,5]
     }
 ]
-find_key=""
-find_value=
-update_key=
-update_value=
+find_key="name"
+find_value="yerassyl2"
+update_key="age"
+update_value=18
 arr = set_data_to_dict(arr,find_key,find_value,update_key,update_value)
 for i in arr:
     print(i)
